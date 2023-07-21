e705a7e688e36abbe733925e9dee153623f777c8
NetWarloq <netwarloq@gmail.com>
2023-07-21 17:12:18 +0500

    add description for varible

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,45 +1,47 @@
 variable "ssh_key_name" {
-    description = "SSH key name"
-    type = string
-    sensitive = true
+  description = "SSH key name"
+  type        = string
+  sensitive   = true
 }
 
 variable "regiondc" {
-    description = "Region DC"
-    type = string
-    sensitive = true
+  description = "Region DC"
+  type        = string
+  sensitive   = true
 }
 
 variable "countdroplets" {
-    description = "Count Droplets"
-    type = number
-    sensitive = true
+  description = "Count Droplets"
+  type        = number
+  sensitive   = true
 }
 
 variable "namepj" {
-    description = "Name project/app"
-    type = string
-    sensitive = true
+  description = "Name project/app"
+  type        = string
+  sensitive   = true
 }
 
 variable "cred_terr_app" {
-    description = "Credential app.terraform"
-    type = string
-    sensitive = true
+  description = "Credential app.terraform"
+  type        = string
+  sensitive   = true
 }
 
 variable "domain_project" {
-    description = "Domain name project"
-    type = string
-    sensitive = true
+  description = "Domain name project"
+  type        = string
+  sensitive   = true
 }
 
 variable "dd_api_key" {
-  type      = string
-  sensitive = true
+  description = "DataDog API key"
+  type        = string
+  sensitive   = true
 }
 
 variable "dd_app_key" {
-  type      = string
-  sensitive = true
+  description = "DataDog APP key"
+  type        = string
+  sensitive   = true
 }
