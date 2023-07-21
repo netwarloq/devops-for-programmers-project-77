001000b51ea6b3d8860d81556ae4a24b892ebdcc
NetWarloq <netwarloq@gmail.com>
2023-07-21 17:38:56 +0500

    add all newline end file..

--- terraform/project.tf
+++ terraform/project.tf
@@ -1,6 +1,6 @@
 resource "digitalocean_project" "newproject" {
-  name        = "${var.namepj}"
+  name        = var.namepj
   description = "A project for infra as code"
   purpose     = "Web Application"
-  resources =  concat([digitalocean_database_cluster.postgres-db-cl.urn, digitalocean_loadbalancer.web-lb.urn, digitalocean_domain.main.urn], tolist(digitalocean_droplet.web.*.urn))
+  resources   = concat([digitalocean_database_cluster.postgres-db-cl.urn, digitalocean_loadbalancer.web-lb.urn, digitalocean_domain.main.urn], tolist(digitalocean_droplet.web.*.urn))
 }
